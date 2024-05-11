65e01ec8e2a66e2c1e23562c9d437b6771744c75
Ankit-akumar <as2262011@gmail.com>
2024-05-11 17:08:42 +0530

    Added login functionality and updated navbar

--- Deployment/Deployment/urls.py
+++ Deployment/Deployment/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Home.urls")),
+    path('', include("Home.urls"), name='home'),
+    path('accounts/', include("django.contrib.auth.urls")),
     #path('pre_deployment/<str:site>/', include("PreDeployment.urls"), name='pre_deployment'),
     path('pre_deployment/', include(("PreDeployment.urls", 'PreDeployment'), namespace='PreDeployment')),
     path('post_deployment/', include(("PostDeployment.urls", 'PostDeployment'), namespace='PostDeployment')),
