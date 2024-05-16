a0ed744f1e447ebb9cbef6d46d66e22fac2eb6d0
Ankit-akumar <as2262011@gmail.com>
2024-05-16 20:32:31 +0530

    commiting files to CloudModelSetup for merge to main

--- Deployment/Home/models.py
+++ Deployment/Home/models.py
@@ -11,10 +11,27 @@ class SiteModel(models.Model):
     knode2_IP_address = models.GenericIPAddressField()
     username = models.CharField(max_length=50, blank=False, help_text="This username will be used to access the servers.")
     password = models.CharField(max_length=50, blank=False, help_text="The password for the username. Will be used to access servers.")
+
     dashboard_url = models.URLField(max_length=200)
     dashboard_username = models.CharField(max_length=50, blank=False, help_text="This username will be used to login to dashboards.")
     dashboard_password = models.CharField(max_length=50, blank=False, help_text="This password will be used to login to dashboards.")
-    is_cloud_site = models.BooleanField(help_text="True if this is a cloud site else if on prem site then False.")
 
+    def __str__(self): 
+        return self.name 
+    
+class CloudSite(models.Model):
+
+    #fields
+    name = models.CharField(max_length=200, unique=True, help_text="The name of the Site. Site name should be unique.")
+    hostname = models.GenericIPAddressField()
+    cluster_name = models.CharField(max_length=50, blank=False, help_text="Cluster to be switched.")
+    namespace_name = models.CharField(max_length=50, blank=False, help_text="Namespace to be switched.")
+    username = models.CharField(max_length=50, blank=False, help_text="This username will be used to access the servers.")
+    password = models.CharField(max_length=50, blank=False, help_text="The password for the username. Will be used to access servers.")
+
+    dashboard_url = models.URLField(max_length=200)
+    dashboard_username = models.CharField(max_length=50, blank=False, help_text="This username will be used to login to dashboards.")
+    dashboard_password = models.CharField(max_length=50, blank=False, help_text="This password will be used to login to dashboards.")
+    
     def __str__(self): 
         return self.name 
